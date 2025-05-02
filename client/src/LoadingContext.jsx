ac8523c5594afdadbcaff4f3c8262f08dfd5b5ec
rbmyres <rmyres3@gmail.com>
2025-05-02 15:16:02 -0500

    LoadingContext toggles after every axios request and contains LoadingSpinner

--- client/src/LoadingContext.jsx
+++ client/src/LoadingContext.jsx
@@ -1,15 +1,47 @@
-import { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
+import axios from "axios";
+import LoadingSpinner from "./Components/LoadingSpinner";
 
 const LoadingContext = createContext();
 
 export const LoadingProvider = ({ children }) => {
-    const [loading, setLoading] = useState(false);
-    
-    return (
-        <LoadingContext.Provider value={{ loading, setLoading }}>
-            {children}
-        </LoadingContext.Provider>
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const reqInterceptor = axios.interceptors.request.use(
+      config => {
+        setLoading(true);
+        return config;
+      },
+      error => {
+        setLoading(false);
+        return Promise.reject(error);
+      }
     );
+
+    const resInterceptor = axios.interceptors.response.use(
+      response => {
+        setLoading(false);
+        return response;
+      },
+      error => {
+        setLoading(false);
+        return Promise.reject(error);
+      }
+    );
+
+    return () => {
+      axios.interceptors.request.eject(reqInterceptor);
+      axios.interceptors.response.eject(resInterceptor);
+    };
+  }, []);
+
+  return (
+    <LoadingContext.Provider value={{ loading, setLoading }}>
+      {loading && <LoadingSpinner />}
+      {children}
+    </LoadingContext.Provider>
+  );
 };
 
 export const useLoading = () => useContext(LoadingContext);
