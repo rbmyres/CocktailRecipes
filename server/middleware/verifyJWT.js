baffc2ad0f70865aed43f686e1567a2c5e5668fb
rbmyres <rmyres3@gmail.com>
2025-05-02 00:16:56 -0500

    debugging jwt and routing

--- server/middleware/verifyJWT.js
+++ server/middleware/verifyJWT.js
@@ -3,7 +3,13 @@ const jwt = require("jsonwebtoken");
 function verifyJWT(req, res, next) {
     const token = req.cookies.token;
 
-    if (!token) return res.sendStatus(401);
+    console.log("Cookie:", req.cookies); 
+    console.log("Token:", token);
+
+    if (!token) {
+        console.log("No token found");
+        return res.status(401).json({ message: "No auth token found" });
+    }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
         if (err) {
