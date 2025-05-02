d40fb9326927c1c8e8e9ca58d7e407842319d465
rbmyres <rmyres3@gmail.com>
2025-05-02 18:48:57 -0500

    updating jwt cookie

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -72,6 +72,7 @@ router.post("/login", (req, res) => {
                     const token = jwt.sign({ id }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
                     
                     res.cookie("token", token, { 
+                        domain: ".client-qfaf.onrender.com/",
                         httpOnly: true,
                         secure: true,
                         sameSite: "none",
